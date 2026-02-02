ceb4e43bf70f2f9674dada57d087a5d97f25761c
quewon <36914642+quewon@users.noreply.github.com>
2026-02-02 12:11:31 +0100

    mobile touch fix

--- touch.js
+++ touch.js
@@ -1,21 +1,34 @@
 import { keydown, keypressed } from "./keyboard.js";
 
+const TOUCH_IGNORE_TIME = 70;
+
+var lTouch;
+var rTouch;
+
 document.addEventListener("touchstart", e => {
-    if (e.touches) e.preventDefault();
+    e.preventDefault();
     for (let touch of e.touches) {
+        touch.timestamp = new Date().getTime();
         if (touch.pageX < window.innerWidth/2) {
-            keydown["LeftTouch"] = true;
-            keypressed["LeftTouch"] = true;
+            if (!lTouch || touch.timestamp - lTouch.timestamp > TOUCH_IGNORE_TIME) {
+                keydown["LeftTouch"] = true;
+                keypressed["LeftTouch"] = true;
+                lTouch = touch;
+            }
         } else if (touch.pageX > window.innerWidth/2) {
-            keydown["RightTouch"] = true;
-            keypressed["RightTouch"] = true;
+            if (!rTouch || touch.timestamp - rTouch.timestamp > TOUCH_IGNORE_TIME) {
+                keydown["RightTouch"] = true;
+                keypressed["RightTouch"] = true;
+                rTouch = touch;
+            }
         }
     }
 })
 document.addEventListener("touchmove", e => {
-    if (e.touches) e.preventDefault();
+    e.preventDefault();
 })
 document.addEventListener("touchend", e => {
+    e.preventDefault();
     for (let touch of e.touches) {
         if (touch.pageX < window.innerWidth/2) {
             delete keydown["LeftTouch"];
