d0a53d0d925695562e70dddfb06383c3d93171b4
quewon <36914642+quewon@users.noreply.github.com>
2026-02-02 19:30:14 +0100

    .

--- beatmap.js
+++ beatmap.js
@@ -22,28 +22,28 @@ var clears = 0;
 var hiscore = 0;
 
 var soundpacks = [
-    // {
-    //     hitSounds: [
-    //         new AudioSprite({ src: "res/packs/doubles/guitar/1/hit.wav" }),
-    //         new AudioSprite({ src: "res/packs/doubles/guitar/2/hit.wav" }),
-    //     ],
-    //     sustainSounds: [
-    //         new AudioSprite({ src: "res/packs/doubles/guitar/1/sustain.wav" }),
-    //         new AudioSprite({ src: "res/packs/doubles/guitar/2/sustain.wav" }),
-    //     ],
-    //     releaseSounds: [
-    //         new AudioSprite({ src: "res/packs/doubles/guitar/1/release.wav" }),
-    //         new AudioSprite({ src: "res/packs/doubles/guitar/2/release.wav" })
-    //     ],
-    //     countdownSounds: [
-    //         new AudioSprite({ src: "res/packs/countdown/newyear/4.wav" }),
-    //         new AudioSprite({ src: "res/packs/countdown/newyear/3.wav" }),
-    //         new AudioSprite({ src: "res/packs/countdown/newyear/2.wav" }),
-    //         new AudioSprite({ src: "res/packs/countdown/newyear/1.wav" }),
-    //     ],
-    //     metronomeSound: new AudioSprite({ src: "res/packs/doubles/guitar/metronome.wav" }),
-    //     countinSound: new AudioSprite({ src: "res/packs/doubles/guitar/countin.wav" }),
-    // },
+    {
+        hitSounds: [
+            new AudioSprite({ src: "res/packs/doubles/guitar/1/hit.wav" }),
+            new AudioSprite({ src: "res/packs/doubles/guitar/2/hit.wav" }),
+        ],
+        sustainSounds: [
+            new AudioSprite({ src: "res/packs/doubles/guitar/1/sustain.wav" }),
+            new AudioSprite({ src: "res/packs/doubles/guitar/2/sustain.wav" }),
+        ],
+        releaseSounds: [
+            new AudioSprite({ src: "res/packs/doubles/guitar/1/release.wav" }),
+            new AudioSprite({ src: "res/packs/doubles/guitar/2/release.wav" })
+        ],
+        countdownSounds: [
+            new AudioSprite({ src: "res/packs/countdown/newyear/4.wav" }),
+            new AudioSprite({ src: "res/packs/countdown/newyear/3.wav" }),
+            new AudioSprite({ src: "res/packs/countdown/newyear/2.wav" }),
+            new AudioSprite({ src: "res/packs/countdown/newyear/1.wav" }),
+        ],
+        metronomeSound: new AudioSprite({ src: "res/packs/doubles/guitar/metronome.wav" }),
+        countinSound: new AudioSprite({ src: "res/packs/doubles/guitar/countin.wav" }),
+    },
     {
         hitSounds: [
             new AudioSprite({ src: "res/packs/doubles/vox/1/hit.wav" }),
@@ -62,28 +62,28 @@ var soundpacks = [
         metronomeSound: new AudioSprite({ src: "res/packs/doubles/vox/metronome.wav" }),
         countinSound: new AudioSprite({ src: "res/packs/doubles/vox/countin.wav" }),
     },
-    // {
-    //     hitSounds: [
-    //         new AudioSprite({ src: "res/packs/doubles/glass/1/hit.wav" }),
-    //         new AudioSprite({ src: "res/packs/doubles/glass/2/hit.wav" })
-    //     ],
-    //     sustainSounds: [
-    //         new AudioSprite({ src: "res/packs/doubles/glass/1/sustain.wav" }),
-    //         new AudioSprite({ src: "res/packs/doubles/glass/2/sustain.wav" })
-    //     ],
-    //     releaseSounds: [
-    //         new AudioSprite({ src: "res/packs/doubles/glass/1/release.wav" }),
-    //         new AudioSprite({ src: "res/packs/doubles/glass/2/release.wav" })
-    //     ],
-    //     countdownSounds: [
-    //         new AudioSprite({ src: "res/packs/doubles/glass/countdown/4.wav" }),
-    //         new AudioSprite({ src: "res/packs/doubles/glass/countdown/3.wav" }),
-    //         new AudioSprite({ src: "res/packs/doubles/glass/countdown/2.wav" }),
-    //         new AudioSprite({ src: "res/packs/doubles/glass/countdown/1.wav" }),
-    //     ],
-    //     metronomeSound: new AudioSprite({ src: "res/packs/doubles/glass/metronome.wav" }),
-    //     countinSound: new AudioSprite({ src: "res/packs/doubles/glass/countin.wav" }),
-    // }
+    {
+        hitSounds: [
+            new AudioSprite({ src: "res/packs/doubles/glass/1/hit.wav" }),
+            new AudioSprite({ src: "res/packs/doubles/glass/2/hit.wav" })
+        ],
+        sustainSounds: [
+            new AudioSprite({ src: "res/packs/doubles/glass/1/sustain.wav" }),
+            new AudioSprite({ src: "res/packs/doubles/glass/2/sustain.wav" })
+        ],
+        releaseSounds: [
+            new AudioSprite({ src: "res/packs/doubles/glass/1/release.wav" }),
+            new AudioSprite({ src: "res/packs/doubles/glass/2/release.wav" })
+        ],
+        countdownSounds: [
+            new AudioSprite({ src: "res/packs/doubles/glass/countdown/4.wav" }),
+            new AudioSprite({ src: "res/packs/doubles/glass/countdown/3.wav" }),
+            new AudioSprite({ src: "res/packs/doubles/glass/countdown/2.wav" }),
+            new AudioSprite({ src: "res/packs/doubles/glass/countdown/1.wav" }),
+        ],
+        metronomeSound: new AudioSprite({ src: "res/packs/doubles/glass/metronome.wav" }),
+        countinSound: new AudioSprite({ src: "res/packs/doubles/glass/countin.wav" }),
+    }
 ]
 var sprites = {
     hitImage: new ImageSprite({ src: "res/images/hit.png" }),
